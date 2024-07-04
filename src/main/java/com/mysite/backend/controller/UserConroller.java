c41fc1eb82d9c3296281288b36f273d1da33b3aa
JBKIM <drv98@naver.com>
2024-07-04 11:31:04 +0900

    유저 업데이트 메소드

--- src/main/java/com/mysite/backend/controller/UserConroller.java
+++ src/main/java/com/mysite/backend/controller/UserConroller.java
@@ -33,4 +33,16 @@ public class UserConroller {
 				.orElseThrow(() -> new UserNotFoundException(id));
     }
 
+    //업데이트 put 메소드
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user); //새로 수정된 유저를 다시 저장
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
 }
